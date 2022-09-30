0c5e0dfeee345e325232f05872e610ffe3d94a0e
unknown <adeelnasirkbw@gmail.com>
2022-09-30 13:13:41 -0700

    logger to log the statements

--- authentications/src/index.ts
+++ authentications/src/index.ts
@@ -1,11 +1,10 @@
 import { app } from './utils/app';
 import config from 'config';
+import { log } from './utils/logger';
 
 const PORT = config.get<number>('port');
 const HOST = config.get<string>('host');
 
 app.listen(PORT, async () => {
-  console.log(
-    `Authentication server is listening on port http://${HOST}:${PORT}`
-  );
+  log.info(`Authentication server is listening on port http://${HOST}:${PORT}`);
 });
