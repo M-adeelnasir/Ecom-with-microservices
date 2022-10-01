01dc5b5d87f5f27744e9d8d235c8e7737e282cfe
unknown <adeelnasirkbw@gmail.com>
2022-09-30 22:39:31 -0700

    authenticate setup complete

--- authentications/src/index.ts
+++ authentications/src/index.ts
@@ -6,7 +6,12 @@ import swaggerDocs from './utils/swagger';
 const PORT = config.get<number>('port');
 const HOST = config.get<string>('host');
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   log.info(`Authentication server is listening on port http://${HOST}:${PORT}`);
   swaggerDocs(app, PORT);
+});
+
+process.on('unhandledRejection', (err) => {
+  log.error('SERVER ERROR ===>', err);
+  server.close(process.exit(1));
 });
--- authentications/src/utils/app.ts
+++ authentications/src/utils/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import Routes from '../routes';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('dev'));
 
 Routes(app);
 
