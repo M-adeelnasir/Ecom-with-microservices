40978bf5f70929eeef2f126571f68a4398bf1fe2
unknown <adeelnasirkbw@gmail.com>
2022-10-12 03:38:34 -0700

    all exports from common module

---
+++ common/src/index.ts
@@ -0,0 +1,10 @@
+//errors exports
+export * from './errors/NotFound.error';
+export * from './errors/badRequest.error';
+export * from './errors/base.error';
+export * from './errors/unAuthorized.error';
+export * from './errors/statusCodes';
+
+//middleware exports
+export * from './middlewares/errorResponse';
+export * from './middlewares/validateRequest';
