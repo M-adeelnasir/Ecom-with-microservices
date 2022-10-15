6c3582cabb650b2577361c06c8c1f218006a2725
unknown <adeelnasirkbw@gmail.com>
2022-10-14 17:17:24 -0700

    contoller for product routes created

---
+++ products/src/controller/sub-category.controller.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from 'express';
+import { get } from 'lodash';
+import { log } from '../utils/logger';
+
+export const exmapleHandler = async (req: Request, res: Response) => {
+  try {
+    //handle logic
+  } catch (err: any) {
+    //hanlde errors
+    log.error(err);
+    throw new Error(err);
+  }
+};
