efed242ee3522bea7b704ac325a7e102cd5032f1
unknown <adeelnasirkbw@gmail.com>
2022-10-15 06:39:40 -0700

    product category controller handler created

--- products/src/controller/category.controller.ts
+++ products/src/controller/category.controller.ts
@@ -1,16 +1,37 @@
 import { Response, Request } from 'express';
 import { get } from 'lodash';
-import {log} from '../utils/logger'
+import {
+  createCategory,
+  deleteCategoryWithSlug,
+  findCategoryWithSlug,
+  getAllCategories,
+  updateCategoryWithSlug,
+} from '../services/category.service';
 
+export const createCategoryHandler = async (req: Request, res: Response) => {
+  const product = await createCategory(req.body);
+  res.status(201).json({ product });
+};
 
-export const exmapleHandler = async (req: Request, res: Response) => {
-try {
-//handle logic
+export const findProductHandler = async (req: Request, res: Response) => {
+  const slug = get(req.params, 'slug');
+  const product = await findCategoryWithSlug(slug);
+  res.json({ product });
+};
 
-} catch (err: any) {
-//hanlde errors
-log.error(err)
-throw new Error(err)
-}
+export const deleteProductHandler = async (req: Request, res: Response) => {
+  const slug = get(req.body, 'slug');
+  await deleteCategoryWithSlug(slug);
+  res.status(204).json({ msg: 'Product Deleted' });
+};
 
-}
+export const getAllProductHandler = async (req: Request, res: Response) => {
+  const products = await getAllCategories();
+  res.json({ products });
+};
+
+export const updateProductHandler = async (req: Request, res: Response) => {
+  const slug = get(req.body, 'slug');
+  const product = await updateCategoryWithSlug(slug, req.body);
+  res.json({ product });
+};
