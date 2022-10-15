30f3a0109dcf0cee425713a72f2106383f4716b1
unknown <adeelnasirkbw@gmail.com>
2022-10-15 05:44:31 -0700

    product mongoose services created

---
+++ products/src/services/product.service.ts
@@ -0,0 +1,52 @@
+import Product, { ProductDocument } from './../model/product.model';
+import { DocumentDefinition, FilterQuery, UpdateQuery } from 'mongoose';
+
+//create product
+export const createProduct = async (
+  input: DocumentDefinition<Omit<ProductDocument, 'createdAt,updatedAt'>>
+) => {
+  const product = await Product.create(input);
+  if (!product) {
+    throw new Error('Something wrong! product create failed');
+  }
+  return product;
+};
+
+//find a product
+export const findProductBySlug = async (
+  slug: FilterQuery<ProductDocument['slug']>
+) => {
+  const product = await Product.findOne({ slug });
+  if (!product) {
+    throw new Error('Something wrong! product create failed');
+  }
+  return product;
+};
+
+//update a product
+export const updateProductBySlug = async (
+  slug: UpdateQuery<ProductDocument['slug']>,
+  data: ProductDocument
+) => {
+  const product = await Product.findOneAndUpdate({ slug }, data, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product) {
+    throw new Error('Something wrong! product update failed');
+  }
+  return product;
+};
+
+//delete a product
+export const deleteProductBySlug = async (
+  slug: FilterQuery<ProductDocument['slug']>
+) => {
+  return await Product.findOneAndDelete({ slug });
+};
+
+//get all products
+export const geAllProductBySlug = async () => {
+  const products = await Product.find({});
+  return products;
+};
