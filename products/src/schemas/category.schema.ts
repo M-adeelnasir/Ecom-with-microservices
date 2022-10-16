de90055d4f4afa68a23b07719e0fe5c460ca01c7
unknown <adeelnasirkbw@gmail.com>
2022-10-15 18:17:07 -0700

    category validation schema configured

--- products/src/schemas/category.schema.ts
+++ products/src/schemas/category.schema.ts
@@ -2,12 +2,24 @@ import { string, object } from 'yup';
 
 export const categorySchema = object({
   body: object({
-    name: string().required('Category Name is required'),
+    name: string()
+      .required('Category Name is required')
+      .min(2, 'Category name should be more then 2 characters')
+      .max(32, 'Category name should be less then 23 characters'),
+  }),
+});
+
+export const findCategory = object({
+  params: object({
+    slug: string().required('Slug is required'),
   }),
 });
 
 export const subCategorySchema = object({
   body: object({
-    name: string().required('Sub category Name is required'),
+    name: string()
+      .required('Sub category Name is required')
+      .min(2, 'Category name should be more then 2 characters')
+      .max(32, 'Category name should be less then 23 characters'),
   }),
 });
